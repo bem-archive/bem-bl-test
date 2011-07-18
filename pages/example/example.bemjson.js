841a6fb50ff85fe940d16fd8926d5234d9baa16a
Varvara Stepanova <toivonen@toivonen.ru>
2011-07-18 13:08:05 -0400

    Use modificator _color_green bu default

--- pages/example/example.bemjson.js
+++ pages/example/example.bemjson.js
@@ -2,15 +2,15 @@
     block: 'b-page',
     title: 'b-link',
     head: [
-        { elem: 'css', url: '/pages/example.css' },
-        { elem: 'css', url: '/pages/example.ie.css', condition: 'lte IE 7' },
+        { elem: 'css', url: 'example.css' },
+        { elem: 'css', url: 'example.ie.css', condition: 'lte IE 7' },
         { elem : 'js', url: '//yandex.st/jquery/1.6.2/jquery.min.js' },
-        { elem: 'js', url: '/pages/example.js' }
+        { elem: 'js', url: 'example.js' }
     ],
     content: [
         {
             block: 'b-link',
-            mods : { pseudo : 'yes', togcolour : 'yes' },
+            mods : { pseudo : 'yes', togcolor : 'yes', color: 'green' },
             url: '#',
             target: '_blank',
             title: 'Кликни меня',
