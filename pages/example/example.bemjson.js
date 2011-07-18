e4b0d0cb7c43d59e9f0286688a39b6a7d2685495
Varvara Stepanova <toivonen@toivonen.ru>
2011-07-18 13:57:36 -0400

    Use conditional comments for ie

--- pages/example/example.bemjson.js
+++ pages/example/example.bemjson.js
@@ -2,8 +2,8 @@
     block: 'b-page',
     title: 'b-link',
     head: [
-        { elem: 'css', url: 'example.css' },
-        { elem: 'css', url: 'example.ie.css', condition: 'lte IE 7' },
+        { elem: 'css', url: 'example.css'},
+        { elem: 'css', url: 'example.ie.css', ie: true },
         { elem : 'js', url: '//yandex.st/jquery/1.6.2/jquery.min.js' },
         { elem: 'js', url: 'example.js' }
     ],
