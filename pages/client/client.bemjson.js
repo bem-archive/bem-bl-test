5eec448336e6731e0468ea4baaf691443e436dc0
Varvara Stepanova <toivonen@yandex-team.ru>
2011-09-10 12:08:24 +0400

    Page for client side templates

---
+++ pages/client/client.bemjson.js
@@ -0,0 +1,11 @@
+({
+    block: 'b-page',
+    title: 'Client-side template work',
+    head: [
+        { elem: 'css', url: 'client.css'},
+        { elem: 'css', url: 'client.ie.css', ie: true },
+        { block: 'i-jquery', elem: 'core' },
+        { elem: 'js', url: 'client.js' }
+    ],
+    content: ''
+})
