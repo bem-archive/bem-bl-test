a06ac178db9eb50ababf4ea56c429faa6c0bc913
Sergey Belov <peimei@ya.ru>
2012-05-03 19:21:20 +0300

    make.js example

--- .bem/make.js
+++ .bem/make.js
@@ -1,25 +1,15 @@
-var n = require('bem').nodes,
-    Node = n.Node,
-    BundlesLevelNode = n.BundlesLevelNode,
-    LibraryNode = n.LibraryNode;
+/*
 
-exports.arch = function() {
-    var all = this.setNode(new Node('all')),
-        build = this.setNode(new Node('build'), all),
-        libs = createBlockLibrariesNodes(this, build);
-    createBundlesLevelsNodes(this, build, libs);
+MAKE.decl('MyCustomNode', MyCustomNode);
 
-    console.log('== Graph on build start ==\n', this.toString());
-};
+MAKE.decl('LevelClassName', {
 
-function createBlockLibrariesNodes(arch, parent) {
-    return [
-        arch.setNode(new LibraryNode('bem-bl', 'git://github.com/bem/bem-bl.git'), parent)
-    ];
-}
+    prop: null,
 
-function createBundlesLevelsNodes(arch, parent, children) {
-    return [
-        arch.setNode(new BundlesLevelNode('pages'), parent, children)
-    ];
-}
+    method: function() {
+        return this.__base();
+    }
+
+});
+
+*/
