d0f4f6ac76e2329ac84ed20aada201fd61c3ca16
Sergey Belov <peimei@ya.ru>
2012-05-15 20:19:27 +0300

    Use css files with expanded @import statements

--- pages/example/example.bemjson.js
+++ pages/example/example.bemjson.js
@@ -2,8 +2,8 @@
     block: 'b-page',
     title: 'Pseudo link',
     head: [
-        { elem: 'css', url: 'example.css'},
-        { elem: 'css', url: 'example.ie.css', ie: 'lt IE 8' },
+        { elem: 'css', url: '_example.css'},
+        { elem: 'css', url: '_example.ie.css', ie: 'lt IE 8' },
         { block: 'i-jquery', elem: 'core' },
         { elem: 'js', url: 'example.js' }
     ],
